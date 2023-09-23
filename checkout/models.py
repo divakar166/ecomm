846a839c845edc89581f743627bb9c460a8e89a6
divakar166 <ds299555@gmail.com>
2023-09-23 15:19:48 +0530

    update

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from base.models import BaseModel
+from django.contrib.auth.models import User
+from accounts.models import Coupon
+from products.models import Product
 # Create your models here.
+class CheckoutCart(BaseModel):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='checkout')
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='product')
+    price = models.IntegerField(blank=True,null=True)
+    quantity = models.IntegerField(default=1)
+    is_paid = models.BooleanField(default=False)
--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from checkout.views import *
 
 urlpatterns = [
-    path('/<uid>/',checkout,name="checkout")
+    path('<uid>/',checkout,name="checkout")
 ]
